c0ce268cc04303572dda9dc68099e64560f64427
ShahrukhSharif <44498230+ShahrukhSharif@users.noreply.github.com>
2019-09-09 19:03:16 +0530

    Update index.php

--- index.php
+++ index.php
@@ -9,28 +9,10 @@ if($method == 'POST'){
 
 	$text = $json->result->parameters->text;
 
-	switch ($text) {
-		case 'hi':
-			$speech = "Hi, Nice to meet you";
-			break;
-
-		case 'bye':
-			$speech = "Bye, good night";
-			break;
-
-		case 'anything':
-			$speech = "Yes, you can type anything here.";
-			break;
-		
-		default:
-			$speech = "Sorry, I didnt get that. Please ask me something else.";
-			break;
-	}
 
 	$response = new \stdClass();
 	$response->speech = $speech;
-	$response->displayText = $speech;
-	$response->source = "webhook";
+
 	echo json_encode($response);
 }
 else
